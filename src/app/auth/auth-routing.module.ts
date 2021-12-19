33ac5ff705a3d6ee90ed36f1b94ebc1f1fbe6e1f
Edison <edison@ucrop.it>
2021-12-19 12:42:54 -0400

    feat: add routing module

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './pages/login/login.component';
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
