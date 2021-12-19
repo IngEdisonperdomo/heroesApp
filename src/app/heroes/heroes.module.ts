85cc3b6f1decac2733de62b81913ce88179b06b9
Edison <edison@ucrop.it>
2021-12-19 13:44:05 -0400

    feat: add routing module heroe

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -5,8 +5,7 @@ import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
-
-
+import { HeroeRoutingModule } from './heroe-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,10 +13,8 @@ import { ListadoComponent } from './pages/listado/listado.component';
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
-    ListadoComponent
+    ListadoComponent,
   ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, HeroeRoutingModule],
 })
-export class HeroesModule { }
+export class HeroesModule {}
